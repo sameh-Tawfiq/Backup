7586b5176edacaeffe94cca86510172aa384c12f
Sameh Tawfik <sameh.tawfk@gmail.com>
2023-08-10 14:10:01 -0700

    message

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,6 @@ int main(void)
 	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongintType));
-	printf("Size of a floatType: %zu byte(s)\n", sizeof(floatType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
         return (0);
 }
