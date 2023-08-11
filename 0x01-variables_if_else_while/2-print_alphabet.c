1aea8527e4f339fcd4df8bce88adf61e38c6f591
Sameh Tawfik <sameh.tawfk@gmail.com>
2023-08-11 09:19:58 -0700

    message

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,7 +6,11 @@
  */
 int main(void)
 {
-	/*char alpha;*/
-	/* for (alpha = 'a'; alpha <= 'z'; alpha++ )*/
-	putchar("abcdefghijklmnopqrstuvwxyz\n");
+	char alpha;
+	for (alpha = 'a'; alpha <= 'z'; alpha++ )
+	{
+		putchar(alpha);
+		putchar("\n");
+	}
+	return(0)
 }
