dfe4b11c6a7302d20eb8f8fc8066b9b9cb5a0e0c
Sameh Tawfik <sameh.tawfk@gmail.com>
2023-08-11 08:46:52 -0700

    message

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* 
- *
- * */
+/**
+ * main - Entry point
+ * Code check n is positive or negative
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
