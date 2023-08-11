544e7f59f9fbdfc1a6140aaa1173c09a499d977e
Sameh Tawfik <sameh.tawfk@gmail.com>
2023-08-11 09:16:04 -0700

    message

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Code prints the alphabet in lower case
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*char alpha;*/
+	/* for (alpha = 'a'; alpha <= 'z'; alpha++ )*/
+	putchar("abcdefghijklmnopqrstuvwxyz\n");
+}
