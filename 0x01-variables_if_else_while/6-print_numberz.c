2786dcd66428254c4a10dfd6ae7dcdbbd4e99938
Sameh Tawfik <sameh.tawfk@gmail.com>
2023-08-11 09:48:35 -0700

    message

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Code prints the alphabet in lower case
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
