1091eef729918f309d1feaf2ed77ec9bf2f4a23c
Sameh Tawfik <sameh.tawfk@gmail.com>
2023-08-16 12:43:27 -0700

    message

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - the function
+ * Description: check character is lowercase or not
+ * Return: 0 or 1
+ */
+int _islower(int c)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (c == alpha)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
