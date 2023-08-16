7fc0ba4e20c58f6ba2ad784ae7473753b165c6f6
Sameh Tawfik <sameh.tawfk@gmail.com>
2023-08-16 12:57:02 -0700

    message

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_sign - function name
+ * Description: print sign of the number
+ * @n: the value it receives
+ * Return: 1, 0, -1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	_putchar('0');
+	return (0);
+}
