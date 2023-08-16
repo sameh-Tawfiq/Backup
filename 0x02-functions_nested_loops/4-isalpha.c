60aad6cc0cf3fbf8144869cc9f4937cb8b069038
Sameh Tawfik <sameh.tawfk@gmail.com>
2023-08-16 12:50:25 -0700

    message

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _isalpha - the function
+ * Description: check if it's alpha or not
+ * @c: the value it receives
+ * Return: 0 or 1
+ */
+int _isalpha(int c)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (c == alpha)
+		{
+			return (1);
+		}
+	}
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
+        {
+                if (c == alpha)
+                {
+                        return (1);
+                }
+        }
+	return (0);
+}
