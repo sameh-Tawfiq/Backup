046282d75305ef259dca46a961ed67f0596ecf5b
Sameh Tawfik <sameh.tawfk@gmail.com>
2023-08-17 09:51:28 -0700

    message

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,6 +11,7 @@ void print_triangle(int size)
 	if (size == 0)
 	{
 		_putchar('\n');
+		_putchar('\n');
 	}
 	else if (size == 1)
 	{
@@ -31,6 +32,5 @@ void print_triangle(int size)
 				_putchar('#');
 			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
