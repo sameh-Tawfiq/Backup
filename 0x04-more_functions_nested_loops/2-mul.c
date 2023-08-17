4dd114c347b06c2194cc9dd42d5d1f2057f453bf
Sameh Tawfik <sameh.tawfk@gmail.com>
2023-08-17 08:24:09 -0700

    message

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* mul - A function that multiples two inegers
+* @a: An input integer
+* @b: An input integer
+* Return: the muli[lication result
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
