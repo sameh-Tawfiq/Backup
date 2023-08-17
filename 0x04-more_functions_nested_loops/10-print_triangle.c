047e933b58cc952c2e9a36652904447d063f62ac
Sameh Tawfik <sameh.tawfk@gmail.com>
2023-08-17 09:38:56 -0700

    message

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_triangle - a function that prints a triangle,
+ * followed by a new line.
+ * @size : An input integer
+ * Return: None
+ */
+void print_triangle(int size)
+{
+	if (size == 0)
+	{
+		_putchar('\n');
+	}
+	else if (size == 1)
+	{
+		_putchar('#');
+	}
+	else
+	{
+		int i;
+
+		for (i = 1; i <= size; i++)
+		{
+			int spaces = size - i;
+			int j;
+
+			for (j = 1; j <= spaces; j++)
+				_putchar(' ');
+			for (j = 1; j <= i; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
