11c52c4dd43b2f1d8777473dc37fc80fe0940513
Sameh Tawfik <sameh.tawfk@gmail.com>
2023-08-21 12:39:02 -0700

    m

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - A function that reverse a string
+ * @s: A pointer to string
+ * Return: Null
+ */
+
+void print_rev(char *s)
+{
+	char *str = s;
+	int index = -1;
+
+	while (*s != '\0')
+	{
+		index++;
+		s++;
+	}
+	while (*(str+index) >= 0)
+	{
+		_putchar(*(str+index));
+		index--;
+	}
+	_putchar('\n');
+}
