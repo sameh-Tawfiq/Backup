aa4eccc1af69a6457d568203a4d41a7e22b7215b
Sameh Tawfik <sameh.tawfk@gmail.com>
2023-08-24 07:44:42 -0700

    m

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reverse_array - A function that reverses the content of an array of integers
+ * @a: Parameter1
+ * @n: Parameter2
+ * Return: None
+ */
+void reverse_array(int *a, int n)
+{
+	int *first = a;
+	int *last = (a + n) - 1;
+	if ( n % 2 == 0)
+	{
+		while( first < last )
+		{
+			int temp;
+
+			temp = *first;
+			*first = *last;
+			*last = temp;
+			first++;
+			last--;
+		}
+
+	}
+	else
+	{
+		while ( first < last )
+		{
+			int temp;
+
+                        temp = *first;
+                        *first = *last;
+                        *last = temp;
+                        first++;
+                        last--;
+                }
+	}
+}
